2359f936c815ac980c43278b63bfedea6b35879a
willriggins <williamriggins2013@gmail.com>
2016-06-24 14:45:06 -0400

    pass hashing, incorrect password alert/redirect

--- src/main/java/com/theironyard/controllers/JobTrackerController.java
+++ src/main/java/com/theironyard/controllers/JobTrackerController.java
@@ -1,11 +1,16 @@
 package com.theironyard.controllers;
 
+import com.theironyard.entities.User;
 import com.theironyard.services.ApplicationRepository;
 import com.theironyard.services.UserRepository;
+import com.theironyard.utils.PasswordStorage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * Created by will on 6/23/16.
@@ -20,8 +25,30 @@ public class JobTrackerController {
     ApplicationRepository applications;
 
     @RequestMapping(path = "/", method = RequestMethod.GET)
-    public String home() {
+    public String home(HttpSession session, Model model) {
+        String username = (String) session.getAttribute("username");
+        model.addAttribute("username", username);
         return "home";
+    }
+
+    @RequestMapping(path = "/login", method = RequestMethod.POST)
+    public String login(String username, String password, HttpSession session) throws Exception {
+        User user = users.findByName(username);
+        if (user == null) {
+            user = new User(username, PasswordStorage.createHash(password));
+            users.save(user);
+        }
+        else if (!PasswordStorage.verifyPassword(password, user.getPassword())) {
+            return "redirect:/error";
+        }
+        session.setAttribute("username", username);
+        return "redirect:/";
+    }
+
+    @RequestMapping(path = "/logout", method = RequestMethod.POST)
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "redirect:/";
     }
 
 }
--- src/main/java/com/theironyard/services/UserRepository.java
+++ src/main/java/com/theironyard/services/UserRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.repository.CrudRepository;
  * Created by will on 6/24/16.
  */
 public interface UserRepository extends CrudRepository<User, Integer> {
+    public User findByName(String name);
 }
