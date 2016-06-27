2bb7c5585e3d73f1186f1dd7ab64385654a06d75
willriggins <williamriggins2013@gmail.com>
2016-06-27 02:05:55 -0400

    more jquery, jar file

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -3,3 +3,11 @@ $(document).ready(function(){
         $(".wrapper1").fadeToggle();
     });
 });
+
+$(function(){
+   $(".toggle").click(function () {
+      $(this).text(function(i, text){
+          return text === "Show" ? "Hide" : "Show";
+      })
+   });
+})
